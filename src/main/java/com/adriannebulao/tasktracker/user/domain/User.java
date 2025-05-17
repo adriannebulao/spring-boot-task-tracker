781e67d9436e199e34d27e295ca85729a598f314
adriannebulao <amcbulao@gmail.com>
2025-05-17 14:18:19 +0800

    feat(user): add user module and user entity

---
+++ src/main/java/com/adriannebulao/tasktracker/user/domain/User.java
@@ -0,0 +1,55 @@
+package com.adriannebulao.tasktracker.user.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    @Setter
+    private String firstName;
+
+    @Column(nullable = false)
+    @Setter
+    private String lastName;
+
+    @Column(nullable = false)
+    @Setter
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    private void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    private void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
