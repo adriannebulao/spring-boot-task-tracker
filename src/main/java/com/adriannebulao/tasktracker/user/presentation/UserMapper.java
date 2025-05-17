14fac04fa448aa78fbe1922c4de45ca7efa963d4
adriannebulao <amcbulao@gmail.com>
2025-05-17 16:32:22 +0800

    fix: silence unmapped target propety build warnings

--- src/main/java/com/adriannebulao/tasktracker/user/presentation/UserMapper.java
+++ src/main/java/com/adriannebulao/tasktracker/user/presentation/UserMapper.java
@@ -1,17 +1,22 @@
 package com.adriannebulao.tasktracker.user.presentation;
 
+import com.adriannebulao.tasktracker.task.presentation.TaskMapper;
 import com.adriannebulao.tasktracker.user.domain.User;
 import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+
+    @Mapping(target = "tasks", ignore = true)
     User toEntity(UserRequestDto userRequestDto);
 
     UserResponseDto toDto(User user);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "tasks", ignore = true)
     void updateUserFromDto(UserRequestDto userRequestDto, @MappingTarget User user);
 }
