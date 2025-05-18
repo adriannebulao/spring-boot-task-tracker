6ef6d1c5d6b9b801fbef4a249b596ab76204a77e
adriannebulao <amcbulao@gmail.com>
2025-05-18 17:26:27 +0800

    test(user): add unit tests for findAll, findById, update, and delete repository methods

--- src/test/java/com/adriannebulao/tasktracker/user/persistence/UserRepositoryTest.java
+++ src/test/java/com/adriannebulao/tasktracker/user/persistence/UserRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.adriannebulao.tasktracker.user.persistence;
 
+import com.adriannebulao.tasktracker.common.exception.UserNotFoundException;
 import com.adriannebulao.tasktracker.user.domain.User;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+import java.util.Optional;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
@@ -16,7 +20,7 @@ public class UserRepositoryTest {
     private UserRepository userRepository;
 
     @Test
-    public void save_shouldSaveUser() {
+    public void save_shouldSaveUser_whenUserExists() {
         User user = User.builder()
                 .firstName("Test")
                 .lastName("Test")
@@ -30,5 +34,93 @@ public class UserRepositoryTest {
         Assertions.assertThat(savedUser.getFirstName()).isEqualTo(user.getFirstName());
         Assertions.assertThat(savedUser.getLastName()).isEqualTo(user.getLastName());
         Assertions.assertThat(savedUser.getImageUrl()).isEqualTo(user.getImageUrl());
+    }
+
+    @Test
+    public void findAll_shouldReturnAllUsers_whenThereAreUsers() {
+        User userOne = User.builder()
+                .firstName("Test")
+                .lastName("Test")
+                .imageUrl("Test")
+                .build();
+
+        User userTwo = User.builder()
+                .firstName("Test")
+                .lastName("Test")
+                .imageUrl("Test")
+                .build();
+
+        userRepository.save(userOne);
+        userRepository.save(userTwo);
+
+        List<User> users = userRepository.findAll();
+
+        Assertions.assertThat(users).isNotNull();
+        Assertions.assertThat(users.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void findById_shouldReturnUser_whenUserExists() {
+        User user = User.builder()
+                .firstName("Test")
+                .lastName("Test")
+                .imageUrl("Test")
+                .build();
+
+        userRepository.save(user);
+
+        User foundUser = userRepository.findById(user.getId()).get();
+
+        Assertions.assertThat(foundUser).isNotNull();
+    }
+
+    @Test
+    public void findById_shouldThrowUserNotFoundException_whenUserNotFound() {
+        Long userId = 1L;
+
+        Assertions.assertThatThrownBy(() -> {
+            userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("User not found"));
+        })
+                .isInstanceOf(UserNotFoundException.class)
+                .hasMessageContaining("User not found");
+    }
+
+    @Test
+    public void update_shouldUpdateUser_whenUserExists() {
+        User user = User.builder()
+                .firstName("Test")
+                .lastName("Test")
+                .imageUrl("Test")
+                .build();
+
+        User savedUser = userRepository.save(user);
+
+        savedUser.setFirstName("Updated");
+        savedUser.setLastName("Updated");
+        savedUser.setImageUrl("Updated");
+
+        User updatedUser = userRepository.save(savedUser);
+
+        Assertions.assertThat(updatedUser).isNotNull();
+        Assertions.assertThat(updatedUser.getId()).isEqualTo(savedUser.getId());
+        Assertions.assertThat(updatedUser.getFirstName()).isEqualTo("Updated");
+        Assertions.assertThat(updatedUser.getLastName()).isEqualTo("Updated");
+        Assertions.assertThat(updatedUser.getImageUrl()).isEqualTo("Updated");
+    }
+
+    @Test
+    public void delete_shouldDeleteUser_whenUserExists() {
+        User user = User.builder()
+                .firstName("Test")
+                .lastName("Test")
+                .imageUrl("Test")
+                .build();
+
+        userRepository.save(user);
+        userRepository.delete(user);
+
+        Optional<User> userOptional = userRepository.findById(user.getId());
+
+        Assertions.assertThat(userOptional).isEmpty();
     }
 }
