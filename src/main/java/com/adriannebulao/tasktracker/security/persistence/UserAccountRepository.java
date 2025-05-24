d60f87161c7d4c8460aec354756efef25c16de1f
adriannebulao <amcbulao@gmail.com>
2025-05-24 15:24:27 +0800

    feat(security): add role and user account repository

---
+++ src/main/java/com/adriannebulao/tasktracker/security/persistence/UserAccountRepository.java
@@ -0,0 +1,13 @@
+package com.adriannebulao.tasktracker.security.persistence;
+
+import com.adriannebulao.tasktracker.security.model.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+    Optional<UserAccount> findByUsername(String username);
+    Boolean existsByUsername(String username);
+}
