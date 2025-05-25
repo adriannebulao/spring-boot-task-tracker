c1c94c4b202bf6c3d64e4148b78ff713eaa91c4a
adriannebulao <amcbulao@gmail.com>
2025-05-25 13:16:16 +0800

    feat(userprofile): add validation for user profile request dto

--- src/main/java/com/adriannebulao/tasktracker/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/adriannebulao/tasktracker/common/exception/GlobalExceptionHandler.java
@@ -4,7 +4,9 @@ import com.adriannebulao.tasktracker.task.domain.TaskNotFoundException;
 import com.adriannebulao.tasktracker.userprofile.domain.UserAccountNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
@@ -12,6 +14,15 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationErrors(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+        return errors;
+    }
 
     @ExceptionHandler(UserAccountNotFoundException.class)
     public ResponseEntity<Map<String, String>> handleUserAccountNotFound(UserAccountNotFoundException e) {
