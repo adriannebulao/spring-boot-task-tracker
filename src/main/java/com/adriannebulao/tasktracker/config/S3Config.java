e6f5dfabea20b4060c5a65cefdef0325d6b1d9c7
adriannebulao <amcbulao@gmail.com>
2025-05-25 15:58:54 +0800

    feat(s3): add aws s3 dependency and initial setup

---
+++ src/main/java/com/adriannebulao/tasktracker/config/S3Config.java
@@ -0,0 +1,34 @@
+package com.adriannebulao.tasktracker.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+
+    @Value("${aws.accessKeyId}")
+    private String awsAccessKeyId;
+
+    @Value("${aws.secretKey}")
+    private String awsSecretKey;
+
+    @Value("${aws.s3.bucket}")
+    private String s3BucketName;
+
+    @Value("${aws.region}")
+    private String awsRegion;
+
+    @Bean
+    public AmazonS3 amazonS3() {
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(awsAccessKeyId, awsSecretKey);
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(awsRegion)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
+}
